f1bf81036a7427eb3d0afc4c53f944b8a8daa0ae
CJ1018 <1427756182@qq.com>
2025-10-18 11:49:12 +0000

    lab01 finished!

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -2,6 +2,14 @@
 #include "ll_cycle.h"
 
 int ll_has_cycle(node *head) {
+		if(head == NULL) return 0;
     /* your code here */
+		node *slow = head;
+		node *fast = head;
+		while(fast != NULL && fast->next != NULL){
+			slow = slow->next;
+			fast = fast->next->next;
+			if(slow == fast) return 1;
+		}
     return 0;
 }
